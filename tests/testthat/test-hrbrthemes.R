091e850ee6fc50bb896d62cbc5d0de134263cbbb
boB Rudis <bob@rud.is>
2017-02-25 11:40:42 -0500

    rejiggering travis config

--- tests/testthat/test-hrbrthemes.R
+++ tests/testthat/test-hrbrthemes.R
@@ -1,6 +1,8 @@
 context("basic functionality")
 test_that("we can do something", {
 
+  library(purrr)
+
   tmp_x1 <- scale_x_comma()
   tmp_x2 <- scale_x_percent()
 
@@ -14,6 +16,51 @@ test_that("we can do something", {
     ggplot2::labs(x="Thsi", y="This", title="This", subtitle="This", caption="Thsi")
 
   expect_message(gg_check(gg_tmp), "misspelled")
+
+  gg_tmp <- ggplot2::ggplot() +
+    ggplot2::labs(x="Thsi", y="This", title="This", subtitle="This", caption="Thsi") %>%
+    gg_check()
+
+  invisible(gg_check(mtcars))
+
+  th <- theme_ipsum(grid="XY", axis="xy")
+  expect_that(th$plot.title$family, equals("Arial Narrow"))
+
+  thrc <- theme_ipsum_rc(grid="XY", axis="xy")
+  expect_that(thrc$plot.title$family, equals("Roboto Condensed"))
+
+  invisible(scale_x_percent(sec.axis = sec_axis(~.+10)))
+  invisible(scale_y_percent(sec.axis = sec_axis(~.+10)))
+  invisible(scale_x_comma(sec.axis = sec_axis(~.+10)))
+  invisible(scale_y_comma(sec.axis = sec_axis(~.+10)))
+
+  invisible(scale_x_percent(sec.axis = ~.+10))
+  invisible(scale_y_percent(sec.axis = ~.+10))
+  invisible(scale_x_comma(sec.axis = ~.+10))
+  invisible(scale_y_comma(sec.axis = ~.+10))
+
+  invisible(theme_ipsum(grid=FALSE))
+  invisible(theme_ipsum_rc(grid=FALSE))
+  invisible(theme_ipsum(grid="XY"))
+  invisible(theme_ipsum_rc(grid="XY"))
+  invisible(theme_ipsum(grid="xy"))
+  invisible(theme_ipsum_rc(grid="xy"))
+
+  invisible(theme_ipsum(axis=TRUE))
+  invisible(theme_ipsum_rc(axis=TRUE))
+
+  invisible(theme_ipsum(axis=FALSE))
+  invisible(theme_ipsum_rc(axis=FALSE))
+  invisible(theme_ipsum(axis="xy"))
+  invisible(theme_ipsum_rc(axis="xy"))
+  invisible(theme_ipsum(axis=""))
+  invisible(theme_ipsum_rc(axis=""))
+
+  invisible(update_geom_font_defaults())
+  invisible(import_roboto_condensed())
+
+  invisible(theme_ipsum(ticks=TRUE))
+  invisible(theme_ipsum_rc(ticks=TRUE))
 
   expect_that(tmp_x1$expand, equals(c(0,0)))
   expect_that(tmp_x2$expand, equals(c(0,0)))
