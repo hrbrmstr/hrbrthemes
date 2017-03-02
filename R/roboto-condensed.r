6896a70c316567f6332939eba1d894066e17c5d3
Bob Rudis <bob@rud.is>
2017-03-02 10:26:44 -0500

    Forgot to update vdiffr tests; tweaked theme(s) base font size to 11.5; updated README

--- R/roboto-condensed.r
+++ R/roboto-condensed.r
@@ -22,6 +22,7 @@
 #' @param caption_family,caption_face,caption_size,caption_margin plot caption family, face, size and margin
 #' @param axis_title_family,axis_title_face,axis_title_size axis title font family, face and size
 #' @param axis_title_just axis title font justificationk one of `[blmcrt]`
+#' @param axis_text_size font size of axis text
 #' @param plot_margin plot margin (specify with [ggplot2::margin])
 #' @param grid_col grid color
 #' @param grid panel grid (`TRUE`, `FALSE`, or a combination of `X`, `x`, `Y`, `y`)
@@ -59,17 +60,18 @@
 #'   theme(axis.text.y=element_blank())
 #' }
 theme_ipsum_rc <- function(
-  base_family="Roboto Condensed", base_size = 11,
+  base_family="Roboto Condensed", base_size = 11.5,
   plot_title_family=base_family, plot_title_size = 18,
   plot_title_face="bold", plot_title_margin = 10,
   subtitle_family=if (.Platform$OS.type == "windows") "Roboto Condensed" else "Roboto Condensed Light",
-  subtitle_size = 12,
+  subtitle_size = 13,
   subtitle_face = "plain", subtitle_margin = 15,
   strip_text_family = base_family, strip_text_size = 12,
   strip_text_face = "plain",
   caption_family=if (.Platform$OS.type == "windows") "Roboto Condensed" else "Roboto Condensed Light",
   caption_size = 9,
   caption_face = "plain", caption_margin = 10,
+  axis_text_size = base_size,
   axis_title_family = base_family, axis_title_size = 9,
   axis_title_face = "plain", axis_title_just = "rt",
   plot_margin = margin(30, 30, 30, 30),
@@ -83,9 +85,9 @@ theme_ipsum_rc <- function(
 
   if (inherits(grid, "character") | grid == TRUE) {
 
-    ret <- ret + theme(panel.grid=element_line(color=grid_col, size=0.10))
-    ret <- ret + theme(panel.grid.major=element_line(color=grid_col, size=0.10))
-    ret <- ret + theme(panel.grid.minor=element_line(color=grid_col, size=0.05))
+    ret <- ret + theme(panel.grid=element_line(color=grid_col, size=0.2))
+    ret <- ret + theme(panel.grid.major=element_line(color=grid_col, size=0.2))
+    ret <- ret + theme(panel.grid.minor=element_line(color=grid_col, size=0.15))
 
     if (inherits(grid, "character")) {
       if (regexpr("X", grid)[1] < 0) ret <- ret + theme(panel.grid.major.x=element_blank())
@@ -134,8 +136,8 @@ theme_ipsum_rc <- function(
   xj <- switch(tolower(substr(axis_title_just, 1, 1)), b=0, l=0, m=0.5, c=0.5, r=1, t=1)
   yj <- switch(tolower(substr(axis_title_just, 2, 2)), b=0, l=0, m=0.5, c=0.5, r=1, t=1)
 
-  ret <- ret + theme(axis.text.x=element_text(margin=margin(t=0)))
-  ret <- ret + theme(axis.text.y=element_text(margin=margin(r=0)))
+  ret <- ret + theme(axis.text.x=element_text(size=axis_text_size, margin=margin(t=0)))
+  ret <- ret + theme(axis.text.y=element_text(size=axis_text_size, margin=margin(r=0)))
   ret <- ret + theme(axis.title=element_text(size=axis_title_size, family=axis_title_family))
   ret <- ret + theme(axis.title.x=element_text(hjust=xj, size=axis_title_size,
                                                family=axis_title_family, face=axis_title_face))
