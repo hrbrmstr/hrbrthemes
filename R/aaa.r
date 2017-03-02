2b79a8c3753473809620cdd2017e02cde4d2eb06
Whan, Alex (PI, Black Mountain) <Alex.Whan@csiro.au>
2017-03-02 15:37:30 +1100

    added font registering .onAttach for win hrbrmstr/hrbrthemes#12

--- R/aaa.r
+++ R/aaa.r
@@ -7,3 +7,11 @@ try_require <- function(package, fun) {
   stop("Package `", package, "` required for `", fun , "`.\n", # nocov start
     "Please install and try again.", call. = FALSE) # nocov end
 }
+
+#from extrafonts
+.onAttach <- function(libname, pkgname) {
+  if (.Platform$OS.type == "windows") {
+    loadfonts("win", quiet = TRUE)
+    packageStartupMessage("Registering fonts with R")
+  }
+}
