2d7a625c59991064ce0e1f43b44d86c762a55909
boB Rudis <bob@rud.is>
2017-11-15 16:35:34 -0500

    trying to fix expand_scales error

--- R/plexsans.R
+++ R/plexsans.R
@@ -180,14 +180,14 @@ theme_ipsum_ps <- function(
 #' @export
 import_plex_sans <- function() {
 
-  rc_font_dir <- system.file("fonts", "plex-sans", package="hrbrthemes")
+  ps_font_dir <- system.file("fonts", "plex-sans", package="hrbrthemes")
 
-  suppressWarnings(suppressMessages(extrafont::font_import(rc_font_dir, prompt=FALSE)))
+  suppressWarnings(suppressMessages(extrafont::font_import(ps_font_dir, prompt=FALSE)))
 
   message(
     sprintf(
       "You will likely need to install these fonts on your system as well.\n\nYou can find them in [%s]",
-      rc_font_dir)
+      ps_font_dir)
   )
 
 }
